2380d183739f6319ae79b2c07c2babe6139ddaee
Marco Tronu <marco.tronu@gmail.com>
2021-08-12 09:27:16 +0200

    update repo

---
+++ utilities/test.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+
+def simplef(x,y):
+    return x + y 
+
+simplef()
+
+for i in range(10):
+    for i in range(25):
+        if i + j < 20:
+            print(i+j)
